30dba68ceab88e55c4c4102e5b4053393d3b82e1
Nguyen Van Long (K15 HL) <longnvhe151244@fpt.edu.vn>
2023-07-02 23:12:32 +0700

    update page admin

--- media-electric-front-end/src/app/admin/admin-routing.module.ts
+++ media-electric-front-end/src/app/admin/admin-routing.module.ts
@@ -1,7 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AdminComponent} from "./admin.component";
+import {BaseComponent} from "./base/base.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {SignInComponent} from "./auth/sign-in/sign-in.component";
+import {SignUpComponent} from "./auth/sign-up/sign-up.component";
+import {UserManagerComponent} from "./user/manager/user-manager.component";
+import {UserEditComponent} from "./user/edit/user-edit.component";
 
 const routes: Routes = [
   {
@@ -9,8 +14,30 @@ const routes: Routes = [
     component: AdminComponent,
     children: [
       {
-        path: '/dashboard',
-        component: DashboardComponent
+        path: '',
+        component: BaseComponent,
+        children: [
+          {
+            path: 'dashboard',
+            component: DashboardComponent,
+          },
+          {
+            path: 'users',
+            component: UserManagerComponent,
+          },
+          {
+            path: 'users/edit',
+            component: UserEditComponent
+          }
+        ]
+      },
+      {
+        path: 'login',
+        component: SignInComponent,
+      },
+      {
+        path: 'register',
+        component: SignUpComponent
       }
     ]
   }
--- media-electric-front-end/src/app/admin/admin.module.ts
+++ media-electric-front-end/src/app/admin/admin.module.ts
@@ -5,12 +5,20 @@ import { AdminRoutingModule } from './admin-routing.module';
 import {AdminComponent} from "./admin.component";
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {SharedModule} from "../shared/shared.module";
+import { BlankComponent } from './blank/blank.component';
+import { BaseComponent } from './base/base.component';
+import { UserManagerComponent } from './user/manager/user-manager.component';
+import { UserEditComponent } from './user/edit/user-edit.component';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
-    DashboardComponent
+    DashboardComponent,
+    BlankComponent,
+    BaseComponent,
+    UserManagerComponent,
+    UserEditComponent
   ],
   imports: [
     CommonModule,
